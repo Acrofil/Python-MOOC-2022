fcfb7c34c1482c789e0f82a3054c44fba15aa25f
Atanas <axpeshev@gmail.com>
2023-01-08 00:32:40 +0200

    part 7, exercise 10 & 11

--- part07-10_valid_pic/src/valid_pic.py
+++ part07-10_valid_pic/src/valid_pic.py
@@ -1,8 +1,57 @@
+from datetime import datetime
+
+def control_character(pic: str, valid: bool):
+
+    valid = None
+    control_chars = "0123456789ABCDEFHJKLMNPRSTUVWXY"
+
+    nine_digits = pic[0:6] + pic[7:10]
+    
+    control_char = int(nine_digits) % 31
+
+    if control_chars[control_char] != pic[-1]:
+        valid = False
+    else:
+        valid= True
+    
+    return valid
+        
+    
 def is_it_valid(pic: str):
 
+    is_it_valid = True
 
+# Check if its correct lenght
+    if len(pic) > 11:
+        is_it_valid = False
+        return is_it_valid
 
+    day = int(pic[0:2])
+    month = int(pic[2:4])
+    
+    if "+" in pic:
+        year =  "18" + pic[4:6]
+        pic_year = int(year)
+    elif "-" in pic:
+        year = "19" + pic[4:6]
+        pic_year = int(year)
+    elif "A" in pic:
+        year = "20" + pic[4:6]
+        pic_year = int(year)
+    else:
+        return is_it_valid == False
+
+#Check if its correct date format
+    try:
+        date = datetime(pic_year, month, day)
+
+    except ValueError: return is_it_valid == False 
+    
+#Check if control character is valid
+    is_it_valid = control_character(pic, is_it_valid)
+
+    return is_it_valid
 
 if __name__ == "__main__":
-    a = "110986+713J"
+    a = "290200-1239"
     print(is_it_valid(a))
--- part07-11_screen_time/src/screen_time.py
+++ part07-11_screen_time/src/screen_time.py
@@ -1,1 +1,86 @@
-# Write your solution here
+from datetime import datetime, timedelta
+#Write to file 
+def write_to_file(filename: str, starting_date: datetime, end_date: datetime, sc_time: int, avg_sc_time: int, days: list, times: tuple):
+#Start & end date format ddmmyy 
+    write_start_date = starting_date.strftime("%d.%m.%Y")
+    write_end_date = end_date.strftime("%d.%m.%Y")
+#Open filename
+    open(filename, "w").close()
+    with open(filename, "a") as my_file:
+        #Write date period, total & avg time
+        my_file.write(f"Time period: {write_start_date}-{write_end_date}\n")
+        my_file.write(f"Total minutes: {sc_time}\n")
+        my_file.write(f"Average minutes: {avg_sc_time}\n")
+#Enumerate over days and times
+        for index, day in enumerate(days):
+            
+            my_file.write(f"{day}: {times[index][0]}/{times[index][1]}/{times[index][2]}\n")
+
+
+def date_format(days_count: int, starting_date):
+
+    days_from_starting_date = timedelta(days=days_count)
+    days_calc = starting_date + days_from_starting_date
+
+    return days_calc
+
+def screen_time():
+
+    filename = input("Filename: ")
+    st_date = input("Starting date: ")
+    total_days = int(input("How many days: "))
+
+
+    starting_date = datetime.strptime(st_date, "%d.%m.%Y")
+    end = timedelta(days=total_days)
+    end2 = timedelta(days=1)
+    end_date = starting_date + end - end2
+
+
+    count = 0
+    total_scr_time = []
+#Add days to a list and times to a list as tuple
+    days = []
+    times = []
+
+    print("Please type in screen time in minutes on each day (TV computer mobile)")
+
+    while count < total_days:
+
+        new_date = date_format(count, starting_date)
+
+        display_date = new_date.strftime("%d.%m.%Y")
+        days.append(display_date)
+#Append days and times
+        scr_time = input(f"Screen time {display_date}:")
+        times.append(tuple(scr_time.split(" ")))
+
+        scr_time_split = scr_time.split(" ")
+        total_scr_time.append(scr_time_split)
+        
+
+        count += 1
+
+#Calculate total screen time and avg sc time    
+    total_screen_time = screen_time_minutes(total_scr_time)
+    average_screen_time = total_screen_time / total_days
+
+    write_to_file(filename, starting_date, end_date, total_screen_time, average_screen_time, days, times)
+
+def screen_time_minutes(scr_time: list):
+
+    total_minutes = 0
+
+    for num in scr_time:
+        print(num)
+
+        for min in num:
+            total_minutes += int(min)
+
+    return total_minutes
+
+def main():
+
+    screen_time()
+
+main()
